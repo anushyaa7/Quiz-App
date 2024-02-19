9a04e015aca196f77fa4bbc4ca85386df50bd430
Anushya <anuselva2000@gamil.com>
2024-02-19 19:10:55 +0530

    first commit

---
+++ src/components/Question.jsx
@@ -0,0 +1,19 @@
+import QuestionTimer from "./QuestionTimer";
+import Answers from "./Answers";
+
+export default function Question({questionText, answers, onSelectAnswer, selectedAnswer, answerState, onSkipAnswer}){
+    return(
+        <div id="question">
+        <QuestionTimer 
+        timeOut={10000} 
+        onTimeOut={onSkipAnswer} />
+
+       <h2>{questionText}</h2>
+       <Answers 
+       answers ={answers} 
+       selectedAnswer={selectedAnswer}
+       answerState={answerState}
+       onSelect={onSelectAnswer} />
+    </div>
+    )
+}
---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export default function QuestionTimer({ timeOut, onTimeOut}){
+ const [remainingTime, setRemainingTime] = useState(timeOut);
+
+ useEffect(() =>{
+    console.log("SETTING TIMEOUT");
+    const timer = setTimeout(onTimeOut, timeOut);
+
+    return() =>{
+        clearTimeout(timer)
+    };
+ }, [timeOut, onTimeOut]);
+
+ useEffect(() =>{
+    console.log("SETTING INTERVAL");
+    const interval = setInterval(() =>{
+        setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+
+    return () => {
+        clearInterval(interval)
+    };
+ }, [])
+
+ return <progress id="question-time" max={timeOut} value={remainingTime} />
+}
